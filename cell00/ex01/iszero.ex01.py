a74a1bc7e1addd889d5f77681e71eff9396f425b
Laddawan3333 <42911@prot.ac.th>
2025-03-11 11:49:54 +0700

    ex01

---
+++ cell00/ex01/iszero.ex01.py
@@ -0,0 +1,10 @@
+number = int(input())
+if number < 0:
+    print("This number is negative.")
+number = int(input())
+if number > 0:
+    print("This number is positive.")
+number = int(input())
+if number == 0:
+    print("This number is both positive and negative")
+    
